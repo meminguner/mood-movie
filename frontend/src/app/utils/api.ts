925f3c11d4fe0c1b2de4c7162314f8bb7dffcfc6
meminguner <59503142+meminguner@users.noreply.github.com>
2025-07-20 23:34:19 +0300

    ananaın am.

--- frontend/src/app/utils/api.ts
+++ frontend/src/app/utils/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL + '/api';
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export interface Film {
   _id: string;
